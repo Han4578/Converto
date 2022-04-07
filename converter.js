dfc7b98deef898673dc4afe93204f54648403359
Han4578 <99018603+Han4578@users.noreply.github.com>
2022-04-07 17:40:22 +0800

    fixed class not recieving input

--- converter.js
+++ converter.js
@@ -1,34 +1,103 @@
-class Temperature{
-    constructor(input,inputUnit,outputUnit){
-        this.input = input
-        this.inputUnit = inputUnit
-        this.outputUnit = outputUnit
+class Temperature {
+    constructor() {
+        this.input
+        this.inputUnit = 'Celsius'
+        this.outputUnit = 'Celsius'
     }
 
-    celciusConvert() {
+    convert() {
+        console.log(this.input)
+        switch (this.inputUnit) {
+            case 'Celsius':
+                this.celsiusConvert()
+                break;
+            case 'Kelvin':
+                this.kelvinConvert()
+                break;
+            case 'Fahrenheit':
+                this.fahrenheitConvert()
+                break;
+            default:
+                return
+        }
+    }
+    celsiusConvert() {
         let input = parseFloat(this.input)
         let output
-        switch(this.outputUnit){
-            case 'Celcius':
+        switch (this.outputUnit) {
+            case 'Celsius':
                 output = input
                 break;
             case 'Kelvin':
                 output = input + 273.15
                 break;
             case 'Fahrenheit':
-                output = (input*9/5) + 32
+                output = (input * 9 / 5) + 32
                 break;
-            default:return
+            default:
+                return
         }
-        
+        this.updateDisplay(output)
+    }
+
+    kelvinConvert() {
+        let input = parseFloat(this.input)
+        let output
+        switch (this.outputUnit) {
+            case 'Celsius':
+                output = input - 273.15
+                break;
+            case 'Kelvin':
+                output = input
+                break;
+            case 'Fahrenheit':
+                output = (input - 273.15) * 9 / 5 + 32
+                break;
+            default:
+                return
+        }
+        this.updateDisplay(output)
+    }
+
+    fahrenheitConvert() {
+        let input = parseFloat(this.input)
+        let output
+        switch (this.outputUnit) {
+            case 'Celsius':
+                output = (input - 32) * 5 / 9
+                break;
+            case 'Kelvin':
+                output = (input - 32) * 5 / 9 + 273.15
+                break;
+            case 'Fahrenheit':
+                output = input
+                break;
+            default:
+                return
+        }
+        this.updateDisplay(output)
+    }
+
+    updateDisplay(output) {
+        document.getElementById('tempOutput').value = output
+        console.log(output)
     }
 }
 
-let measurementValue = document.getElementById('measurement').value
-let tempInput = document.getElementById('tempInput').value
-let tempUnitInput = document.getElementById('tempUnitInput').value
-let tempUnitOutput = document.getElementById('tempUnitOutput').value
+let measurementValue = document.getElementById('measurement')
+let tempInput = document.getElementById('tempInput')
+let tempUnitInput = document.getElementById('tempUnitInput')
+let tempUnitOutput = document.getElementById('tempUnitOutput')
 
-const temperature = new Temperature(tempInput,tempUnitInput,tempUnitOutput)
+const temperature = new Temperature()
 
-
+tempInput.addEventListener('input', (e) => {
+    temperature.input = e.target.value
+})
+tempUnitInput.addEventListener('onchange', (e) => {
+    temperature.inputUnit = e.target.value
+})
+tempUnitOutput.addEventListener('onChange', (e) => {
+    temperature.outputUnit = e.target.value
+    console.log(e)
+})
