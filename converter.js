e5d3c7c314ffb89bd87b8fdcc76127d30850833e
Han4578 <99018603+Han4578@users.noreply.github.com>
2022-04-19 18:27:10 +0800

    Generalizing code
    making variables less specific to avoid repeating code, added distance list and base class

--- converter.js
+++ converter.js
@@ -1,12 +1,12 @@
 class Temperature {
     constructor() {
         this.input
-        this.inputUnit = 'Celsius'
+        this.inputUnit = 'Fahrenheit'
         this.outputUnit = 'Celsius'
     }
 
     convertToKelvin() {
-        if(isNaN(this.input)) return
+        if (isNaN(this.input)) return
         let input = parseFloat(this.input);
         let output
         switch (this.inputUnit) {
@@ -15,7 +15,7 @@ class Temperature {
                 break;
             case 'Kelvin':
                 output = input
-                
+
                 break;
             case 'Fahrenheit':
                 output = (input - 32) * 5 / 9 + 273.15
@@ -25,7 +25,7 @@ class Temperature {
         }
         this.convertToOutput(output)
     }
-    
+
     convertToOutput(input) {
         let output
         switch (this.outputUnit) {
@@ -45,28 +45,122 @@ class Temperature {
     }
 
     updateDisplay(output) {
-        document.getElementById('tempOutput').value = output
+        document.getElementById('output').value = output
+    }
+}
+class Distance {
+    constructor() {
+        this.input
+        this.inputUnit
+        this.outputUnit = unitOutputValue
+    }
+
+    convertToKelvin() {
+        if (isNaN(this.input)) return
+        let input = parseFloat(this.input);
+        let output
+        switch (this.inputUnit) {
+            case 'Celsius':
+                output = input + 273.15
+                break;
+            case 'Kelvin':
+                output = input
+
+                break;
+            case 'Fahrenheit':
+                output = (input - 32) * 5 / 9 + 273.15
+                break;
+            default:
+                return
+        }
+        this.convertToOutput(output)
+    }
+
+    convertToOutput(input) {
+        let output
+        switch (this.outputUnit) {
+            case 'Celsius':
+                output = input - 273.15
+                break;
+            case 'Kelvin':
+                output = input
+                break;
+            case 'Fahrenheit':
+                output = (input - 273.15) * 9 / 5 + 32
+                break;
+            default:
+                return
+        }
+        this.updateDisplay(output)
+    }
+
+    updateDisplay(output) {
+        document.getElementById('output').value = output
     }
 }
 
 let measurementValue = document.getElementById('measurement')
-let tempInput = document.getElementById('tempInput')
-let tempUnitInput = document.getElementById('tempUnitInput')
-let tempUnitOutput = document.getElementById('tempUnitOutput')
+let input = document.getElementById('input')
+let unitInput = document.querySelectorAll('.unitInput')
+let unitOutput = document.querySelectorAll('.unitOutput')
+let swapButton = document.querySelector('.swap')
+let selection = document.querySelectorAll('.selection')
+let inputValue
+let unitInputValue = document.querySelector('.active.unitInput').value
+let unitOutputValue = document.querySelector('.active.unitOutput').value
 
-const temperature = new Temperature()
+const temperature = new Temperature() 
+const distance = new Distance() 
 
-tempInput.addEventListener('input', (e) => {
-    if (e.target.value.slice(e.target.value.length - 1) == '') return
-    temperature.input = e.target.value
-    temperature.convertToKelvin()
+input.addEventListener('input', e => {
+    if (e.target.value.slice(e.target.value.distance - 1) == '') return;
+    inputValue = e.target.value;
+    convert()
 })
-tempUnitInput.addEventListener('input', (e) => {
-    temperature.inputUnit = e.target.value
-    temperature.convertToKelvin()
+
+unitInput.forEach(i => {
+    i.addEventListener('input', e => {
+        unitInputValue = e.target.value;
+        convert()
+
+    })
 
 })
-tempUnitOutput.addEventListener('input', (e) => {
-    temperature.outputUnit = e.target.value
-    temperature.convertToKelvin()
+
+unitOutput.forEach(o => {
+    o.addEventListener('input', e => {
+        unitOutputValue = e.target.value;
+        convert()
+
+    })
+
 })
+
+swapButton.addEventListener('click', () => {
+    let input = document.querySelector('.active.unitInput').value
+    let output = document.querySelector('.active.unitOutput').value
+    document.querySelector('.active.unitOutput').value = input
+    document.querySelector('.active.unitInput').value = output
+    unitOutputValue = input
+    unitInputValue = output
+    convert()
+})
+
+function convert() {
+    switch (document.querySelector('#measurement').value) {
+        case('temperature'):
+        temperature.input = inputValue
+        temperature.inputUnit = unitInputValue
+        temperature.outputUnit = unitOutputValue
+        temperature.convertToKelvin()
+        break;
+        case('distance'):
+        distance.input = inputValue
+        distance.inputUnit = unitInputValue
+        distance.outputUnit = unitOutputValue
+       // distance.convertToMeter()
+        break;
+        default:return
+    }
+}
+
